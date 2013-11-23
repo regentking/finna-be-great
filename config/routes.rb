598cca6ca950d560f458a5fa7d6cde8dea07fa7f
Eric Holl <notreallyfamousyet@gmail.com>
2013-11-23 16:08:14 -0500

    controller magic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Tutsblog::Application.routes.draw do
+
+  resources :posts
+
+  root to: "wecome#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

8d7ded8281a9d3991f4b5059ac9937681253e85e
Eric Holl <notreallyfamousyet@gmail.com>
2013-11-23 15:50:20 -0500

    Just the app now

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tutsblog::Application.config.secret_key_base = '91a161e06d667e9a614250c0576cce2ee6df0f5ee14a2fd057d99ec5f009e4aa0b0cc8172985b9ae6ec3eeffabc2e4752b5f42fd8a908440f569e47429c4c433'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tutsblog::Application.config.session_store :cookie_store, key: '_tutsblog_session'

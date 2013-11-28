45f2d7acd1fe99f81c124828ce91aadb228f7f6d
Eric Holl <notreallyfamousyet@gmail.com>
2013-11-28 11:35:28 -0500

    Added pom.xml

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'f1d1dcde29a25108630b882949b265642844422221f7c696169f4e974a0dad63e8e518573e4925ec918d73d41a123b601fe6f65fbac2234340a2464fd7c76d13'
